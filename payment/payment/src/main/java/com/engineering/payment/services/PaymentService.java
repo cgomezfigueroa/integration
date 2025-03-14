861076d3037a6ef3a585aae136c872f865f97c91
aromerogonzalez <aromerogonzalez@deloitte.com>
2025-03-14 09:26:27 -0600

    Payment: Add kafka and consumed endpoint, coverage 30

--- payment/payment/src/main/java/com/engineering/payment/services/PaymentService.java
+++ payment/payment/src/main/java/com/engineering/payment/services/PaymentService.java
@@ -3,15 +3,21 @@ package com.engineering.payment.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.client.RestTemplate;
 import com.engineering.payment.entities.Payment;
 import com.engineering.payment.repositories.PaymentRepository;
-
-import jakarta.validation.Valid;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class PaymentService {
@@ -19,12 +25,12 @@ public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
-    // Responsibilities: Manages payment processing for orders.
-    // Endpoints Consumed:
-    // PUT /orders/{orderId} from Order Management Service: Update the order status
-    // after payment is processed.
+    @Autowired
+    private RestTemplate restTemplate;
 
-    public ResponseEntity<Payment> paymentDetailsbyId(Integer id) {
+    private String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.w-S-G4v0dTE5r8PajRF756FdYryec6AC2mxomZ7yzVY";
+
+    public ResponseEntity<Payment> paymentDetailsbyId(Long id) {
         Optional<Payment> optionalPayment = paymentRepository.findById(id);
         if (optionalPayment.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -32,9 +38,49 @@ public class PaymentService {
         return ResponseEntity.status(HttpStatus.OK).body(optionalPayment.get());
     }
 
-    public Payment createPayment(@Valid @RequestBody Payment payment) {
+    public Payment createPayment(@RequestBody Payment payment) {
         Payment savedPayment = paymentRepository.save(payment);
+        try {
+            updateOrderStatus(savedPayment.getOrderId());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         return savedPayment;
+    }
+
+    public void updateOrderStatus(Long orderId) throws JsonMappingException, JsonProcessingException {
+        String endPoint = "http://localhost:8080/orders/" + orderId;
+        JsonNode payLoad = modifyOrderStatus(orderId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", token);
+
+        HttpEntity<JsonNode> entity = new HttpEntity<>(payLoad, headers);
+        ResponseEntity<Void> response = restTemplate.exchange(endPoint, HttpMethod.PUT, entity, Void.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            System.out.println("Update successful");
+        } else {
+            System.out.println("Update failed");
+        }
+    }
+
+    public JsonNode modifyOrderStatus(Long orderId) throws JsonMappingException, JsonProcessingException {
+        String endPoint = "http://localhost:8080/orders/" + orderId;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(endPoint, HttpMethod.GET, entity, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode payLoad = mapper.readTree(response.getBody());
+        ((ObjectNode) payLoad).put("status", "Paid");
+
+        return payLoad;
     }
 
 }
